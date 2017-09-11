caf4fd4123a4888dbcf4b89427af572bc2953d5b
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2017-09-11 20:15:29 +0530

    Create albha.c

---
+++ albha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int ch;
+ printf("enter any character:");
+ scanf("%c",&ch);
+ if((c>='a'&& c<='z')//(c>='A'&& c<='Z'))
+ {
+  printf("albhabet");
+  }
+  else
+  {
+   printf("not an albhabet");
+   }
+   return 0;
+   }
