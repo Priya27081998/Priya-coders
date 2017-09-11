b072f17e6c52c22b93bd173613d7a29b498e5bb9
Priya27081998 <31529960+Priya27081998@users.noreply.github.com>
2017-09-11 05:47:38 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ int num;
+ printf("enter an integer:");
+ scanf("%d",num);
+ if(num>0)
+ printf("positive number");
+ elseif(num<0)
+ printf("negative number");
+ else(num=0)
+ printf("number is zero");
+ return 0;
+ }
